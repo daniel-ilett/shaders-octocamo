7f138174f1c36243c7438ca630d4df2dbb98dbc6
Daniel Ilett <danielilett@gmail.com>
2020-10-30 01:02:17 +0000

    Added basic movement & animation.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private PlayerController player;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothTime = 0.2f;
+
+    private Vector3 currentVelocity;
+
+    private void Update()
+    {
+        var targetPosition = player.transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothTime);
+        transform.LookAt(player.transform);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private Collider standingCollider;
+    [SerializeField] private Collider proneCollider;
+
+    private bool isMoving = false;
+    private bool isProne = false;
+
+    private Animator animator;
+    private new Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            isProne = !isProne;
+            animator.SetBool("IsProne", isProne);
+
+            standingCollider.enabled = !isProne;
+            proneCollider.enabled = isProne;
+        }
+
+        var x = Input.GetAxis("Horizontal");
+        var z = Input.GetAxis("Vertical");
+
+        var move = new Vector3(x, 0.0f, z);
+
+        rigidbody.velocity = transform.TransformVector(move);
+
+        isMoving = Mathf.Abs(x) > 0.05f || Mathf.Abs(z) > 0.05f;
+        animator.SetBool("IsMoving", isMoving);
+    }
+}
